21c3d4eabc70777daf5b30a9ef13e25ee2fa7a0b
Robyn <robyn.dalgleish@groupbyinc.com>
2019-01-15 16:38:20 -0500

    Add members to Listing class

--- src/main/java/com/groupbyinc/robyn/exercise/ExerciseApplication.java
+++ src/main/java/com/groupbyinc/robyn/exercise/ExerciseApplication.java
@@ -3,8 +3,8 @@ package com.groupbyinc.robyn.exercise;
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
-        Listing listing1 = new Listing("123", "New Shiny Apartment!");
-        Listing listing2 = new Listing("124", "New Shiny Apartment!");
+        Listing listing1 = new Listing("E999999", "New Shiny Apartment!", "big shiny new house");
+        Listing listing2 = new Listing("E999999", "New Shiny Apartment!");
         System.out.println(listing1.equals(listing2));
 	}
 }
--- src/main/java/com/groupbyinc/robyn/exercise/Listing.java
+++ src/main/java/com/groupbyinc/robyn/exercise/Listing.java
@@ -1,12 +1,38 @@
 package com.groupbyinc.robyn.exercise;
 
-import lombok.Value;
-
-@Value
 public class Listing {
 
-    // String or int is not the good answer! Find out why! Hint: The answer is in my original slack message!
-    String mlsNumber;
+    private final String mlsNumber;
+    private final String title;
+    private final String description;
 
-    String title;
+    public Listing(String mlsNumber, String title) {
+        this(mlsNumber, title, "");
+    }
+
+    public Listing(String mlsNumber, String title, String description) {
+        if (!isValidMlsNumber(mlsNumber)) {
+            throw new IllegalArgumentException(mlsNumber + " is not a valid MLS number");
+        }
+        this.mlsNumber = mlsNumber;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getMlsNumber() {
+        return mlsNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    private boolean isValidMlsNumber(String string) {
+        return string != null && string.matches("^(?=.*[A-Z]{1})(?=.*[0-9]{6})[A-Z0-9]+$");
+    }
 }
